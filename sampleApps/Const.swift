8f60d99a9a4a02dd99905743b4446698b67829e2
ikedasouichirou <ikedasouichirou@ikedasouichirou-no-MacBook-Air.local>
2015-11-04 17:32:42 +0900

    no message

---
+++ sampleApps/Const.swift
@@ -0,0 +1,9 @@
+//
+//  Const.swift
+//  sampleApps
+//
+//  Created by ikedasouichirou on 2015/11/04.
+//  Copyright © 2015年 ikedasouichirou. All rights reserved.
+//
+
+import Foundation
---
+++ sampleApps/TargetUI.swift
@@ -0,0 +1,25 @@
+//
+//  CircleGraph.swift
+//  sampleApps
+//
+//  Created by ikedasouichirou on 2015/11/04.
+//  Copyright © 2015年 ikedasouichirou. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+/*class CircleGraph
+{
+    let baseCircle:CAShapeLayer
+    let baseShadoeCircle:CAShapeLayer
+    let graphCircle:CAShapeLayer
+    let targetName:UILabel
+    let currentValues:UILabel
+    let targetValues:UILabel
+    
+    init()
+    {
+    }
+    
+}*/
